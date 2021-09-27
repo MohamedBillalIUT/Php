c2bd25e724835adc53003a46c66b1ebc66997953
MohamedBillalIUT <billal.mohamed@etu.umontpellier.fr>
2021-09-27 15:18:55 +0200

    first push

---
+++ TD2/testConf.php
@@ -0,0 +1,16 @@
+<?php
+    //include Conf::getLogin();
+
+
+    // On inclut les fichiers de classe PHP avec require_once
+    // pour éviter qu'ils soient inclus plusieurs fois
+    require_once 'Conf.php';
+
+    // On affiche le login de la base de donnees
+    echo Conf::getLogin();
+    echo Conf::getDatabase();
+    echo Conf::getHostname();
+    echo Conf::getpassword();
+
+
+?>

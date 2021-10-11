5eb007aa0aa9b8f4770a7560a49bc97488b4c2b0
MohamedBillalIUT <billal.mohamed@etu.umontpellier.fr>
2021-10-11 16:01:30 +0200

    TD1 pas fini

--- public_html/php/TD1/Billal1.php
+++ public_html/php/TD1/Billal1.php
@@ -7,6 +7,7 @@
 <body>
 <h1> I can do it</h1>
 <?php
+require_once "TD1/echo.php";
     //Essaye d'afficher une phrase pour commencer
     /*et tqt
     on fera plus dur la prochaine fois */
@@ -32,47 +33,87 @@
     ça me dérange pas car je sais que je finirais d'entreprendre un jour pas d'soucie.
     Paragraphe;
 
-    //LES TABLEAUX (Trés important)
-    // Tableau vide
-    $coeur = array();
 
-    //création de tableau case par case
-    // Association entre => prenom='clé/index'; French='valeur';
-    $mon_tableau['prenom']='French';
-    $mon_tableau['nom']='what';
+// On met la chaine de caractères "hello" dans la variable 'texte'
+$texte2 = "hello world !";// Les noms de variable commencent par $ en PHP
 
-    //Autre association possible
-    $help=array(
-            'prenom'=>'moi',
-            'nom'=>'solidarité',
-            'Pour'=> 'Rosa Parks');
+// On écrit le contenu de la variable 'texte' dans la page Web
+echo $texte2;
 
-    //rajouter un éléments à la fin du tableau
-    $mon_tableau[]= "new Valeur";
+//Parcourir un tablezu d'entier
+for ($i=0; $i<count($mon_tableau); $i++){
+    echo $mon_tableau[$i];
+}
 
-    //Parcourir un tableau de caractère
-    foreach ($mon_tableau as $cle => $valeur){
-        /*foreach vas parcourir toutes les association en en mettant chaque cle d'asso dans la variable $cle
-        et les valeur dans $valeur*/
-        echo "$cle : $valeur\n";
-        // ":" veut dire affiche un apres l'autre
-    }
+/*en gros foreach en mode
+foreach ($mon_tableau as $cle => $valeur){
+//commandes
+}
 
-    //Parcourir un tablezu d'entier
-    for ($i=0; $i<count($mon_tableau); $i++){
-        echo $mon_tableau[$i];
-    }
+c'est équivalent à ça
+for ($i = 0; $i < count(array_keys($mon_tableau)); $i++) {
+$cle = array_keys($mon_tableau)[$i];
+$valeur = $mon_tableau[$cle];
+//commandes }*/
 
-    /*en gros foreach en mode
-    foreach ($mon_tableau as $cle => $valeur){
-    //commandes
-    }
+//LES TABLEAUX (Trés important)
+// Tableau vide
+$coeur = array();
 
-    c'est équivalent à ça
-    for ($i = 0; $i < count(array_keys($mon_tableau)); $i++) {
-    $cle = array_keys($mon_tableau)[$i];
-    $valeur = $mon_tableau[$cle];
-    //commandes }*/
+//création de tableau case par case
+// Association entre => prenom='clé/index'; French='valeur';
+$mon_tableau['prenom']='French';
+$mon_tableau['nom']='what';
+
+//Autre association possible
+$help=array(
+    'prenom'=>'moi',
+    'nom'=>'solidarité',
+    'Pour'=> 'Rosa Parks');
+
+//rajouter un éléments à la fin du tableau
+$mon_tableau[]= "new Valeur";
+
+//<p> Voiture 256AB34 de marque Renault (couleur bleu) </p>
+$marque=null; //initialisation de variable directement en null pour être sur que la variable soit bien initialisé
+$couleur=null; //same
+$immatriculation='';//pas null car clé primaire
+
+$immatriculation='256AB34'; //initialisation des variable avec des valeur
+$marque='Renault';
+$couleur='bleu';
+$texte="Voiture $immatriculation de marque $marque (couleur $couleur)";
+echo $texte;
+
+$voiture=array(
+            $marque,
+            $couleur,
+            $immatriculation);
+
+$voiture['marque']='Renaut';
+$voiture['immatriculation']='256AB34';
+$voiture['couleur']='bleu';
+
+foreach ($voiture as $cle => $valeur){
+
+    $voiture[0]='Renault'||'Ford'||'Lamborgini';//change une variable local
+    $voiture[1]='256AB34'||'216458A'||'215A985';//change une variable local
+    $voiture[2]='bleu'||'noir'||'blanc';
+}
+
+//Parcourir un tableau de caractère
+foreach ($mon_tableau as $cle => $valeur){
+    /*foreach vas parcourir toutes les association en en mettant chaque cle d'asso dans la variable $cle
+    et les valeur dans $valeur*/
+    echo "$cle : $valeur\n";
+    // ":" veut dire affiche un apres l'autre
+}
+for ($j=0; $j<count($voiture2);$j++){//Faire un tableau d'entier
+    $voiture2[$j]=$voiture||$voiture;
+    echo $voiture2[$j];
+}
+var_dump($voiture2);
+
 
 
 ?>
--- public_html/php/TD1/echo.php
+++ public_html/php/TD1/echo.php
@@ -11,12 +11,50 @@
           // Ceci est un commentaire PHP sur une ligne
           /* Ceci est le 2ème type de commentaire PHP
           sur plusieurs lignes */
-           
-          // On met la chaine de caractères "hello" dans la variable 'texte'
-          $texte = "hello world !";// Les noms de variable commencent par $ en PHP
+        //<p> Voiture 256AB34 de marque Renault (couleur bleu) </p>
 
-          // On écrit le contenu de la variable 'texte' dans la page Web
-          echo $texte;
+
+        $marque=null;
+        $couleur=null;
+        $immatriculation='';
+
+        $immatriculation='256AB34';
+        $marque='Renault';
+        $couleur='bleu';
+        $texte="Voiture $immatriculation de marque $marque (couleur $couleur)";
+        echo $texte;
+           
+        // On met la chaine de caractères "hello" dans la variable 'texte'
+        $texte2 = "hello world !";// Les noms de variable commencent par $ en PHP
+
+        // On écrit le contenu de la variable 'texte' dans la page Web
+        echo $texte2;
+
+        $voiture=array(
+            $marque,
+            $couleur,
+            $immatriculation);
+
+        $voiture['marque']='Renaut';
+        $voiture['immatriculation']='256AB34';
+        $voiture['couleur']='bleu';
+
+        foreach ($voiture as $cle => $valeur){
+
+            $voiture[0]='Renault'||'Ford'||'Lamborgini';//change une variable local
+            $voiture[1]='256AB34'||'216458A'||'215A985';//change une variable local
+            $voiture[2]='bleu'||'noir'||'blanc';
+        }
+        $voiture2[]=$voiture;
+
+        for ($j=0; $j<count($voiture2);$j++){//Faire un tableau d'entier
+            $voiture2[$j]=$voiture||$voiture;
+            echo $voiture2[$j];
+        }
+        var_dump($voiture2);
+
         ?>
+
+
     </body>
 </html> 
